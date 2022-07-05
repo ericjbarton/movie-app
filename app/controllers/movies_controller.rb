dcc24585d06c740142d55430b28c8229a99de2fc
Eric Barton <brumpo@Erics-MacBook-Pro.local>
2022-07-05 09:41:56 -0400

    adds order querie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,9 @@ class MoviesController < ApplicationController
     movie = Movie.find_by(id: params[:id])
     render json: movie.as_json
   end
+
+  def order
+    movies = Movie.order("title ASC").reorder("year DESC")
+    render json: movies.as_json
+  end
 end
