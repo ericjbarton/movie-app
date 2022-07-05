02e9e07e43fb22f13e1548047434738b46693c7e
Eric Barton <brumpo@Erics-MacBook-Pro.local>
2022-07-05 09:12:29 -0400

    adds seeds, and everything else. Forgot to commit earlier.

---
+++ app/controllers/actor_controller.rb
@@ -0,0 +1,6 @@
+class ActorController < ApplicationController
+  def one_actor
+    actor = Actor.find_by(id: params["id"])
+    render json: actor.as_json
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
-actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-actor.save
+# actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+# actor.save
+
+movie = Movie.new({ title: "Apollo 10", year: 1993, plot: "The crew survive a harrowing encounter with a floating poop" })
+movie.save
+
+movie = Movie.new({ title: "Tom Hanks Movie", year: 19999, plot: "It's a tom hanks movie. You probably saw it. Your mom loves it, and you do too" })
+movie.save
+
+movie = Movie.new({ title: "Overboard", year: 1987, plot: "Underrated Goldie Hawn classic and my mom's favorite movie" })
+movie.save
