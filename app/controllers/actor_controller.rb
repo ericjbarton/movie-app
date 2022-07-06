000532928f1fcdaaa84f68c15dd3260c39cf0217
Eric Barton <brumpo@Erics-MacBook-Pro.local>
2022-07-06 10:08:33 -0400

    adds actor query

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -3,4 +3,9 @@ class ActorController < ApplicationController
     actor = Actor.find_by(id: params["id"])
     render json: actor.as_json
   end
+
+  def actor_query
+    actor = params["actor"]
+    render json: (actor.as_json)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
   get "/movies/" => "movies#order"
+  get "/actors/" => "actors#actor_query"
 end
