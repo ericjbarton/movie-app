151046a9ebf048430e20f760fe16637bf97486c4
ericjbarton <erockbart@gmail.com>
2022-07-19 08:29:19 -0400

    adds associations, but movie.genres currently doesn't work.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,17 @@
 class Movie < ApplicationRecord
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   validates :title, presence: true
   validates :year, length: { minimum: 4 }
   validates :plot, length: { minimum: 10 }
   validates :director, presence: true
   has_many :actors
+
+  def genre_names
+    genre_array = []
+    genres.each do |genre|
+      genre_array << genre.name
+    end
+    p genre_array
+  end
 end
